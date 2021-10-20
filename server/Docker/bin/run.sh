45101917da5f5a203e219119aca144a953d9190f
Alberto Monteiro <alberto.costa.monteiro@gmail.com>
2021-10-20 12:06:48 -0300

    Ajust code

--- server/Docker/bin/run.sh
+++ server/Docker/bin/run.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-echo "server.properties=${SERVER_PROPERTIES}
+echo "hardware.mqtt.port=${HARDWARE_MQTT_PORT}
+http.port=${HTTP_PORT}
+https.port=${HTTPS_PORT}
+server.ssl.cert=${SERVER_SSL_CERT}
+server.ssl.key=${SERVER_SSL_KEY}
+server.ssl.key.pass=${SERVER_SSL_KEY_PASS}
+server.properties=${SERVER_PROPERTIES}
 mail.properties=${MAIL_PROPERTIES}
 web.request.max.size=${WEB_REQUEST_MAX_SIZE}
 user.tags.limit=${USER_TAGS_LIMIT}
@@ -13,7 +19,6 @@ notifications.frequency.user.quota.limit=${NOTIFICATIONS_FREQUENCY_USER_QUOTA_LI
 user.profile.max.size=${USER_PROFILE_MAX_SIZE}
 profile.save.worker.period=${PROFILE_SAVE_WORKER_PERIOD}
 data.folder=${DATA_FOLDER}
-server.ssl.cert=${SERVER_SSL_CER}
 user.message.quota.limit=${USER_MESSAGE_QUOTA_LIMIT}
 enable.raw.db.data.store=${ENABLE_RAW_DB_DATA_STORE}
 enable.db=${ENABLE_DB}
@@ -23,13 +28,10 @@ stats.print.worker.period=${STATS_PRINT_WORKER_PERIOD}
 log.level=${LOG_LEVEL}
 restore.host=${RESTORE_HOST}
 lcd.strings.pool.size=${LCD_STRINGS_POOL_SIZE}
-hardware.mqtt.port=${HARDWARE_MQTT_PORT}
 admin.pass=${ADMIN_PAS}
 allowed.administrator.ips=${ALLOWED_ADMINISTRATOR_IPS}
 listen.address=${LISTEN_ADDRES}
-http.port=${HTTP_PORT}
 restore=${RESTORE}
-server.ssl.key=${SERVER_SSL_KE}
 admin.rootPath=${ADMIN_ROOTPATH}
 product.name=${PRODUCT_NAME}
 csv.export.data.points.max=${CSV_EXPORT_DATA_POINTS_MAX}
@@ -37,7 +39,6 @@ allow.store.ip=${ALLOW_STORE_IP}
 force.port.80.for.csv=${FORCE_PORT_80_FOR_CSV}
 hard.socket.idle.timeout=${HARD_SOCKET_IDLE_TIMEOUT}
 async.logger.ring.buffer.size=${ASYNC_LOGGER_RING_BUFFER_SIZE}
-https.port=${HTTPS_PORT}
 logs.folder=${LOGS_FOLDER}
 webhooks.frequency.user.quota.limit=${WEBHOOKS_FREQUENCY_USER_QUOTA_LIMIT}
 initial.energy=${INITIAL_ENERGY}
@@ -45,21 +46,20 @@ blocking.processor.thread.pool.limit=${BLOCKING_PROCESSOR_THREAD_POOL_LIMIT}
 table.rows.pool.size=${TABLE_ROWS_POOL_SIZE}
 admin.email=${ADMIN_EMAIL}
 map.strings.pool.size=${MAP_STRINGS_POOL_SIZE}
-server.ssl.key.pass=${SERVER_SSL_KEY_PAS}
 user.dashboard.max.limit=${USER_DASHBOARD_MAX_LIMIT}
 webhooks.response.size.limit=${WEBHOOKS_RESPONSE_SIZE_LIMIT}
 terminal.strings.pool.size=${TERMINAL_STRINGS_POOL_SIZE}
 notifications.queue.limit=${NOTIFICATIONS_QUEUE_LIMIT}
 " >/config/server.properties
 
-echo "mail.smtp.username=${MAIL_SMTP_USERNAME}
-mail.smtp.port=${MAIL_SMTP_PORT}
-mail.smtp.password=${MAIL_SMTP_PASSWORD}
-mail.smtp.auth=${MAIL_SMTP_AUTH}
-mail.smtp.starttls.enable=${MAIL_SMTP_STARTTLS_ENABLE}
-mail.smtp.timeout=${MAIL_SMTP_TIMEOUT}
+echo "mail.smtp.auth=${MAIL_SMTP_AUTH}
+mail.smtp.starttls.enable=${MAIL_SMTP_STARTTLS}
 mail.smtp.host=${MAIL_SMTP_HOST}
+mail.smtp.port=${MAIL_SMTP_PORT}
+mail.smtp.username=${MAIL_SMTP_USERNAME}
+mail.smtp.password=${MAIL_SMTP_PASSWORD}
 mail.smtp.connectiontimeout=${MAIL_SMTP_CONNECTIONTIMEOUT}
+mail.smtp.timeout=${MAIL_SMTP_TIMEOUT}
 " >/config/mail.properties
 
 echo "jdbc.url=${JDBC_URL}
@@ -79,4 +79,4 @@ if [ ! -f "$FILE" ]; then
   curl -L https://github.com/Alberto-Monteiro/blynk-server/releases/download/v"${BLYNK_SERVER_VERSION}"/server-"${BLYNK_SERVER_VERSION}".jar >"$FILE"
 fi
 
-java -jar "$FILE" -dataFolder /data -serverConfig /config/server.properties -mailConfig /config/mail.properties
+exec java -jar "$FILE" -dataFolder /data -serverConfig /config/server.properties -mailConfig /config/mail.properties
